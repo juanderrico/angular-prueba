de8ef0e7a23c5d46d30882f3c7678164a4e360fe
juan derrico <juancruzderrico@hotmail.com>
2022-07-29 14:08:35 -0300

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'prueba';
+
+  baseDeDatos = [new Persona("juan",45),new Persona("alfred",432)]
+  imagenEliminar = "assets/images/eliminar-icono.png"
+  imagenEditar = "assets/images/editar-icono.png"
+  nombreAgregar:string | null=null;
+  dniAgregar:number | null=null;
+  inputblanco:boolean=false;
+  agregarADatos(){
+
+    this.baseDeDatos.push(new Persona(this.nombreAgregar,this.dniAgregar))
+    this.nombreAgregar=null;
+    this.dniAgregar=null;
+
+  }
+  eliminarPerfil(object:Persona){
+    let index= this.baseDeDatos.indexOf(object)
+    this.baseDeDatos.splice(index,1);
+  }
+  editarPerfil(){
+  }
+  comprobarDni(){
+    let active=false;
+    for(let list of this.baseDeDatos){
+
+      if(list.dni==this.dniAgregar){
+        active=true
+        return true
+      }
+
+        }  return active
+    }
+    inputBlanco(){
+      if(this.dniAgregar==null || this.nombreAgregar==null){
+        return true
+      }
+      else{return false}
+    }
+
+
 }
+export class Persona{
+ dni;
+ nombre;
+
+  constructor(n:string | null,d:number | null) {
+    this.dni=d;
+    this.nombre=n;
+  }}
